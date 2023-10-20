b6bca182b7407ba793fba84412b6de7f6ccfb1d7
varsh06pm <varsha.pm123@gmail.com>
2023-10-20 14:36:25 +0530

    init commit

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mongoose =  require('mongoose');
+mongoose.set('strictQuery', true);
+
+const connectDB = async () => {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {  
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+};
+
+module.exports = connectDB; 
--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectdb = require('./config/db');
 
 //load env vars
 dotenv.config({path: './config/config.env' });
 
-const app = express();  //app is the instance of express servee
+//connect to database
+connectdb();
+
+//route files
+const bootcamps = require('./routes/bootcamps');
+
+const app = express();  //app is the instance of express server
+
+//Body parser
+app.use(express.json());
+
+//dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+//mount routers
+app.use('/api/v1/bootcamps',bootcamps);
 
 //to keep the server running
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+//Handle unhandled promise rejections
+process.on('unhandleRejection', (err,promise) =>{
+   console.log(`Error: ${err.message}`);
+   //close the server and exit process
+   server.close(() => process.exit(1));   
+});
